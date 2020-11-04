981e7160a1a6cdda1e613cc5c83a0ee8543e204d
Chrysaoros <s.stojanovic963@gmail.com>
2020-11-04 23:22:18 +0100

    Added LocalStorage to save theme, refactored code

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// ELEMENTS 
+// ELEMENTS
 
 // Navbar elements
 const NAVBAR = document.getElementById("nav");
 const TOGGLE_TEXT_AND_ICON = document.getElementById("toggle-icon");
-const TOGGLE_SWITCH = document.querySelector('input[type=checkbox]');
+const TOGGLE_SWITCH = document.querySelector("input[type=checkbox]");
 
 // About Section images
 const CODER_IMAGE = document.getElementById("coder-image");
@@ -17,41 +17,48 @@ const PROJECT_TEXT = document.getElementById("text-box");
 
 // Change image colors depending on the site theme
 function changeImageColor(color) {
-    CODER_IMAGE.src = `img/undraw_proud_coder_${color}.svg`;
-    PROBLEM_IMAGE.src = `img/undraw_feeling_proud_${color}.svg`;
-    IDEA_IMAGE.src = `img/undraw_conceptual_idea_${color}.svg`;
+  CODER_IMAGE.src = `img/undraw_proud_coder_${color}.svg`;
+  PROBLEM_IMAGE.src = `img/undraw_feeling_proud_${color}.svg`;
+  IDEA_IMAGE.src = `img/undraw_conceptual_idea_${color}.svg`;
 }
 
-// Dark Mode theme
-function darkMode() {
-    NAVBAR.style.backgroundColor = "rgb(0 0 0 / 50%)";
-    PROJECT_TEXT.style.backgroundColor = "rgb(255 255 255 / 50%)";
-    TOGGLE_TEXT_AND_ICON.children[0].textContent = "Dark Mode";
-    TOGGLE_TEXT_AND_ICON.children[1].classList.replace("fa-sun", "fa-moon");
+// Toggle Dark/Light theme
+function toggleDarkLightTheme(isThemeDark) {
+  NAVBAR.style.backgroundColor =
+    isThemeDark === "dark" ? "rgb(0 0 0 / 50%)" : "rgb(255 255 255 / 50%)";
+  PROJECT_TEXT.style.backgroundColor =
+    isThemeDark === "dark" ? "rgb(255 255 255 / 50%)" : "rgb(0 0 0 / 50%)";
+  TOGGLE_TEXT_AND_ICON.children[0].textContent =
+    isThemeDark === "dark" ? "Dark Mode" : "Light Mode";
+  isThemeDark === "dark"
+    ? TOGGLE_TEXT_AND_ICON.children[1].classList.replace("fa-sun", "fa-moon")
+    : TOGGLE_TEXT_AND_ICON.children[1].classList.replace("fa-moon", "fa-sun");
 
-    changeImageColor("dark");
+  changeImageColor(isThemeDark);
 }
 
-// Light Mode theme
-function lightMode() {
-    NAVBAR.style.backgroundColor = "rgb(255 255 255 / 50%)";
-    PROJECT_TEXT.style.backgroundColor = "rgb(0 0 0 / 50%)";
-    TOGGLE_TEXT_AND_ICON.children[0].textContent = "Light Mode";
-    TOGGLE_TEXT_AND_ICON.children[1].classList.replace("fa-moon", "fa-sun");
+// Dark/Light Mode switch
+function siteThemeSwitch(event) {
+  let theme = "light";
 
-    changeImageColor("light");
-}
+  if (event.target.checked) theme = "dark";
 
-// Switch to Dark/Light Mode
-function switchSiteTheme(event) {
-   if (event.target.checked) {
-       document.documentElement.setAttribute("data-theme", "dark");
-       darkMode();
-   } else {
-       document.documentElement.setAttribute("data-theme", "light");
-       lightMode();
-   }
+  document.documentElement.setAttribute("data-theme", theme);
+  localStorage.setItem("theme", theme);
+  toggleDarkLightTheme(theme);
 }
 
 // Event Listener
-TOGGLE_SWITCH.addEventListener("change", switchSiteTheme);
+TOGGLE_SWITCH.addEventListener("change", siteThemeSwitch);
+
+// Save current theme to Local Storage
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+
+  if (currentTheme === "dark") {
+    TOGGLE_SWITCH.checked = true;
+    toggleDarkLightTheme(currentTheme);
+  }
+}
