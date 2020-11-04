2d374be8338d5339c13e2906f67281d4717c0645
Chrysaoros <s.stojanovic963@gmail.com>
2020-11-05 00:11:13 +0100

    Additional minor code cleanup

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ const IDEA_IMAGE = document.getElementById("idea-image");
 // Project Section textbox
 const PROJECT_TEXT = document.getElementById("text-box");
 
+// Theme colors
+const DARK_THEME = "dark";
+const LIGHT_THEME = "light";
 //--------------------------------------------------------------------------
 
 // Change image colors depending on the site theme
@@ -23,25 +26,25 @@ function changeImageColor(color) {
 }
 
 // Toggle Dark/Light theme
-function toggleDarkLightTheme(isThemeDark) {
+function toggleDarkLightTheme(isTheme) {
   NAVBAR.style.backgroundColor =
-    isThemeDark === "dark" ? "rgb(0 0 0 / 50%)" : "rgb(255 255 255 / 50%)";
+    isTheme === DARK_THEME ? "rgb(0 0 0 / 50%)" : "rgb(255 255 255 / 50%)";
   PROJECT_TEXT.style.backgroundColor =
-    isThemeDark === "dark" ? "rgb(255 255 255 / 50%)" : "rgb(0 0 0 / 50%)";
+    isTheme === DARK_THEME ? "rgb(255 255 255 / 50%)" : "rgb(0 0 0 / 50%)";
   TOGGLE_TEXT_AND_ICON.children[0].textContent =
-    isThemeDark === "dark" ? "Dark Mode" : "Light Mode";
-  isThemeDark === "dark"
+    isTheme === DARK_THEME ? "Dark Mode" : "Light Mode";
+  isTheme === DARK_THEME
     ? TOGGLE_TEXT_AND_ICON.children[1].classList.replace("fa-sun", "fa-moon")
     : TOGGLE_TEXT_AND_ICON.children[1].classList.replace("fa-moon", "fa-sun");
 
-  changeImageColor(isThemeDark);
+  changeImageColor(isTheme);
 }
 
 // Dark/Light Mode switch
 function siteThemeSwitch(event) {
-  let theme = "light";
+  let theme = LIGHT_THEME;
 
-  if (event.target.checked) theme = "dark";
+  if (event.target.checked) theme = DARK_THEME;
 
   document.documentElement.setAttribute("data-theme", theme);
   localStorage.setItem("theme", theme);
@@ -57,7 +60,7 @@ const currentTheme = localStorage.getItem("theme");
 if (currentTheme) {
   document.documentElement.setAttribute("data-theme", currentTheme);
 
-  if (currentTheme === "dark") {
+  if (currentTheme === DARK_THEME) {
     TOGGLE_SWITCH.checked = true;
     toggleDarkLightTheme(currentTheme);
   }
